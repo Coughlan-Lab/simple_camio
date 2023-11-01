16eca4ff560d8e185a904200dc7a372abf502723
rcrabb-ski <79174367+rcrabb-ski@users.noreply.github.com>
2023-11-01 16:24:55 -0700

    Added separate functions for loading parameters, for annotating image with axes and backprojected points, and for playing sound. Includes other small revisions.

--- simple_camio.py
+++ simple_camio.py
@@ -10,9 +10,60 @@ import pyglet.media
 from scipy import stats
 
 
-# Function to sort corners by id based on how they are arranged
-def sort_corners_by_id(corners, ids, id_list, scene):
+# Function to load camera intrinsic parameters from a JSON file
+def load_camera_parameters(filename):
+    if os.path.isfile(filename):
+        with open(filename, 'r') as f:
+            cam_params = json.load(f)
+            print("loaded camera parameters from file.")
+    else:
+        print("No camera parameters file found. Please run simple_calibration.py script.")
+        exit(0)
+    intrinsic_matrix = np.array([[cam_params['focal_length_x'], 0.0, cam_params['camera_center_x']],
+                                 [0.00000000e+00, cam_params['focal_length_y'], cam_params['camera_center_y']],
+                                 [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]], dtype=np.float32)
+    return intrinsic_matrix
+
+
+# Function to load map parameters from a JSON file
+def load_map_parameters(filename):
+    if os.path.isfile(filename):
+        with open(filename, 'r') as f:
+            map_params = json.load(f)
+            print("loaded map parameters from file.")
+    else:
+        print("No map parameters file found. Please run simple_calibration.py script.")
+        exit(0)
+    return map_params['model']
+
+
+# function to check if pointer location is within threshold of Z dimension, and if so, play a sound
+def check_and_play_sound(point_of_interest, zone, model, prev_zone_name, start_time):
+    # Check if the Z position is within the threshold, if so, play a sound
+    Z_threshold_cm = 2.0
+    if np.abs(point_of_interest[2]) < Z_threshold_cm and zone > -1:
+        zone_name = model['hotspots'][zone]['textDescription']
+        if prev_zone_name != zone_name:
+            soundfile = model['hotspots'][zone]['audioDescription']
+            if os.path.exists(soundfile) and time.time() - start_time > 0.5:
+                sound = pyglet.media.load(soundfile, streaming=False)
+                # player.next_source()
+                # player.queue(sound)
+                # player.play()
+                try:
+                    sound.play()
+                except(BaseException):
+                    print("Exception raised. Cannot play sound. Please restart the application.")
+                start_time = time.time()
+        return zone_name, start_time
+    return prev_zone_name, start_time
+
+# Function to sort corners by id based on the order specified in the id_list,
+# such that the scene array matches the obj array in terms of aruco marker ids
+# and the appropriate corners.
+def sort_corners_by_id(corners, ids, id_list):
     use_index = np.zeros(len(id_list)*4, dtype=bool)
+    scene = np.empty((len(id_list) * 4, 2), dtype=np.float32)
     for i in range(len(corners)):
         id = ids[i, 0]
         if id in id_list:
@@ -53,6 +104,23 @@ def drawAxes(img, imgpts):
     return img
 
 
+# Draws axes and projects the 3D points onto the image
+def annotate_image(img_scene_color, obj, rvec, tvec, intrinsic_matrix):
+    # Draws the 3D points on the image
+    backprojection_pts, other = cv.projectPoints(obj, rvec, tvec, intrinsic_matrix, None)
+    for idx, pts in enumerate(backprojection_pts):
+        cv.circle(img_scene_color, (int(pts[0, 0]), int(pts[0, 1])), 4, (255, 255, 255), 2)
+        cv.line(img_scene_color, (int(pts[0, 0] - 1), int(pts[0, 1])), (int(pts[0, 0]) + 1, int(pts[0, 1])),
+                (255, 0, 0), 1)
+        cv.line(img_scene_color, (int(pts[0, 0]), int(pts[0, 1]) - 1), (int(pts[0, 0]), int(pts[0, 1]) + 1),
+                (255, 0, 0), 1)
+    # Draw axes on the image
+    axis = np.float32([[6, 0, 0], [0, 6, 0], [0, 0, -6], [0, 0, 0]]).reshape(-1, 3)
+    axis_pts, other = cv.projectPoints(axis, rvec, tvec, intrinsic_matrix, None)
+    img_scene_color = drawAxes(img_scene_color, axis_pts)
+    return img_scene_color
+
+
 # Retrieves the zone of the point of interest on the map
 def get_zone(point_of_interest, img_map, pixels_per_cm):
     x = int(point_of_interest[0] * pixels_per_cm)
@@ -89,13 +157,13 @@ def get_dict_idx_from_color(list_of_dicts, color):
 
 # Parses the list of aruco codes and returns the 2D points and ids
 def parse_aruco_codes(list_of_aruco_codes):
-    obj = np.empty((len(list_of_aruco_codes)*4,3), dtype=np.float32)
+    obj_array = np.empty((len(list_of_aruco_codes)*4,3), dtype=np.float32)
     ids = []
     for cnt, aruco_code in enumerate(list_of_aruco_codes):
         for i in range(4):
-            obj[cnt*4+i,:] = aruco_code['position'][i]
+            obj_array[cnt*4+i,:] = aruco_code['position'][i]
         ids.append(aruco_code['id'])
-    return obj, ids
+    return obj_array, ids
 
 
 # Returns the dictionary code for the given string
@@ -115,12 +183,8 @@ def get_arcuo_dict_from_string(aruco_dict_string):
     elif aruco_dict_string == "DICT_5X5_250":
         return cv.aruco.DICT_5X5_250
 
-#========================================
-pixels_per_cm_obj = 118.49  # text-with-aruco.png
-focal_length_x = 1.88842395e+03
-focal_length_y = 1.89329463e+03
-camera_center_x = 9.21949329e+02
-camera_center_y = 3.34464319e+02
+
+# ========================================
 distortion = np.array([0.09353041, -0.12232207, 0.00182885, -0.00131933, -0.30184632], dtype=np.float32) * 0
 use_external_cam = 0
 #========================================
@@ -129,33 +193,27 @@ parser = argparse.ArgumentParser(description='Code for CamIO.')
 parser.add_argument('--input1', help='Path to input zone image.', default='UkraineMap.json')
 args = parser.parse_args()
 
-if os.path.isfile('camera_parameters.json'):
-    with open('camera_parameters.json', 'r') as f:
-        cam_params = json.load(f)
-        print("loaded camera parameters from file.")
-else:
-    print("No camera parameters file found. Please run simple_calibration.py script.")
-    exit(0)
-intrinsic_matrix = np.array([[cam_params['focal_length_x'], 0.0, cam_params['camera_center_x']],
-                             [0.00000000e+00, cam_params['focal_length_y'], cam_params['camera_center_y']],
-                             [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]], dtype=np.float32)
 
-# Zone filter logic
-prev_zone_name = None
+# Setting up a ring buffer to be a temporal filter for the zone selection. It
+# will keep track of the last [zone_filter_size] zones and return the mode.
 zone_filter_size = 10
 zone_filter = -1*np.ones(zone_filter_size, dtype=int)
 zone_filter_cnt = 0
+prev_zone_name = None
 
-# Load map parameters
-with open(args.input1, 'r') as fp:
-    map_params_data = json.load(fp)
-model = map_params_data['model']
+# Load map and camera parameters
+model = load_map_parameters(args.input1)
+intrinsic_matrix = load_camera_parameters('camera_parameters.json')
+
 # Load color image
 img_map_color = cv.imread(model['filename'], cv.IMREAD_COLOR)  # Image.open(cv.samples.findFile(args.input1))
-img_map = cv.cvtColor(img_map_color, cv.COLOR_BGR2GRAY)
 
 obj, list_of_ids = parse_aruco_codes(model['positioningData']['arucoCodes'])
-scene = np.empty((len(list_of_ids)*4, 2), dtype=np.float32)
+# Define aruco marker dictionary and parameters object to include subpixel resolution
+aruco_dict_scene = cv.aruco.Dictionary_get(get_arcuo_dict_from_string(model['positioningData']['arucoType']))
+aruco_dict_marker = cv.aruco.Dictionary_get(cv.aruco.DICT_5X5_50)
+arucoParams = cv.aruco.DetectorParameters_create()
+arucoParams.cornerRefinementMethod = cv.aruco.CORNER_REFINE_SUBPIX
 player = pyglet.media.Player()
 cap = cv.VideoCapture(use_external_cam)
 start_time = time.time()
@@ -175,14 +233,11 @@ while cap.isOpened():
     # load images grayscale
     img_scene = cv.cvtColor(img_scene_color, cv.COLOR_BGR2GRAY)
 
-    # Define aruco marker dictionary and parameters object to include subpixel resolution
-    aruco_dict = cv.aruco.Dictionary_get(get_arcuo_dict_from_string(model['positioningData']['arucoType']))
-    arucoParams = cv.aruco.DetectorParameters_create()
-    arucoParams.cornerRefinementMethod = cv.aruco.CORNER_REFINE_SUBPIX
-    # Detect aruco markers in image
-    (corners, ids, rejected) = cv.aruco.detectMarkers(img_scene, aruco_dict, parameters=arucoParams)
-    scene, use_index = sort_corners_by_id(corners, ids, list_of_ids, scene)
+    # Detect aruco markers for map in image
+    (corners, ids, rejected) = cv.aruco.detectMarkers(img_scene, aruco_dict_scene, parameters=arucoParams)
+    scene, use_index = sort_corners_by_id(corners, ids, list_of_ids)
 
+    # If no  markers found, show image and continue to next iteration
     if ids is None or not any(use_index):
         print("No markers found.")
         cv.imshow('image reprojection', img_scene_color)
@@ -197,27 +252,11 @@ while cap.isOpened():
     # Run solvePnP using the markers that have been observed
     retval, rvec, tvec = cv.solvePnP(obj[use_index, :], scene[use_index, :], intrinsic_matrix, None)
 
-    # Draw axes on the image
-    axis = np.float32([[6, 0, 0], [0, 6, 0], [0, 0, -6], [0, 0, 0]]).reshape(-1, 3)
-    axis_pts, other = cv.projectPoints(axis, rvec, tvec, intrinsic_matrix, None)
-    img_scene_color = drawAxes(img_scene_color, axis_pts)
+    # Annotate image with 3D points and axes
+    img_scene_color = annotate_image(img_scene_color, obj, rvec, tvec, intrinsic_matrix)
 
-    # Draw circles on the backprojected corner points
-    backprojection_pts, other = cv.projectPoints(obj, rvec, tvec, intrinsic_matrix, None)
-    for idx, pts in enumerate(backprojection_pts):
-        cv.circle(img_scene_color, (int(pts[0, 0]), int(pts[0, 1])), 4, (255, 255, 255), 2)
-        cv.line(img_scene_color, (int(pts[0, 0] - 1), int(pts[0, 1])), (int(pts[0, 0]) + 1, int(pts[0, 1])),
-                (255, 0, 0), 1)
-        cv.line(img_scene_color, (int(pts[0, 0]), int(pts[0, 1]) - 1), (int(pts[0, 0]), int(pts[0, 1]) + 1),
-                (255, 0, 0), 1)
-        # cv.line(img_scene_color, (int(pts[0,0]), int(pts[0,1])), (int(scene[idx,0]), int(scene[idx,1])), (0, 255, 0), 1)
-
-    # Set up aruco detection for the pointer marker
-    aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_5X5_50)
-    dp = cv.aruco.DetectorParameters_create()
-    dp.cornerRefinementMethod = cv.aruco.CORNER_REFINE_SUBPIX
-
-    corners, ids, _ = cv.aruco.detectMarkers(img_scene, aruco_dict, parameters=dp)
+    # Detect aruco marker for pointer in image
+    corners, ids, _ = cv.aruco.detectMarkers(img_scene, aruco_dict_marker, parameters=arucoParams)
     obj_aruco = np.empty((4, 3), dtype=np.float32)
     scene_aruco = np.empty((4, 2), dtype=np.float32)
 
@@ -227,6 +266,8 @@ while cap.isOpened():
     obj_aruco[2, :] = [3.5, 3.5, 0]
     obj_aruco[3, :] = [0.5, 3.5, 0]
 
+    # If marker is detected, draw it on the image and copy the corner points to the scene aruco array for SolvePnP,
+    # otherwise show image and continue to next iteration.
     if len(corners) > 0:
         for i in range(4):
             scene_aruco[i, :] = corners[0][0][i]
@@ -252,33 +293,14 @@ while cap.isOpened():
     point_of_interest = reverse_project(tvec_aruco, rvec, tvec)
 
     # Filter the zones by returning the mode of the last [zone_filter_size] zones
-    zone_color = get_zone(point_of_interest, img_map_color, pixels_per_cm_obj)
+    zone_color = get_zone(point_of_interest, img_map_color, model['pixels_per_cm'])
     zone_filter[zone_filter_cnt] = get_dict_idx_from_color(model['hotspots'], zone_color)
     zone_filter_cnt = (zone_filter_cnt + 1) % zone_filter_size
     zone = stats.mode(zone_filter).mode[0]
 
     # Check if the Z position is within the threshold, if so, play a sound
-    Z_threshold_cm = 2.0
-    if np.abs(point_of_interest[2]) < Z_threshold_cm and zone > -1:
-        zone_name = model['hotspots'][zone]['textDescription']
-        if prev_zone_name != zone_name:
-            soundfile = model['hotspots'][zone]['audioDescription']
-            if os.path.exists(soundfile) and time.time() - start_time > 0.5:
-                sound = pyglet.media.load(soundfile, streaming=False)
-                # player.next_source()
-                # player.queue(sound)
-                # player.play()
-                try:
-                    sound.play()
-                except(BaseException):
-                    print("Exception raised. Cannot play sound. Please restart the application.")
-                start_time = time.time()
-                #playsound(soundfile, block=False)
-        prev_zone_name = zone_name
-        print(zone_name)
-    else:
-        pass
-        #prev_zone_name = None
+    prev_zone_name, start_time = check_and_play_sound(point_of_interest, zone, model, prev_zone_name, start_time)
+
     # print(point_of_interest)#, dist, current_region)
 
     now = datetime.datetime.now()

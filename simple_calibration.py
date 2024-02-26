cc54466e26f33ab7841b7e41bd33dcf1a7673be5
Matteo-3033 <matteo.manzoni2001@gmail.com>
2024-02-26 10:56:33 +0100

    add camera selection in simple_calibration.py

--- simple_calibration.py
+++ simple_calibration.py
@@ -106,6 +106,47 @@ def load_map_parameters(filename):
     return map_params['model']
 
 
+def select_cam_port():
+    available_ports, working_ports, non_working_ports = list_ports()
+    if len(working_ports) == 1:
+        return working_ports[0][0]
+    elif len(working_ports) > 1:
+        print("The following cameras were detected:")
+        for i in range(len(working_ports)):
+            print(f'{i}) Port {working_ports[i][0]}: {working_ports[i][1]} x {working_ports[i][2]}')
+        cam_selection = input("Please select which camera you would like to use: ")
+        return working_ports[int(cam_selection)][0]
+    else:
+        return 0
+
+
+def list_ports():
+    """
+    Test the ports and returns a tuple with the available ports and the ones that are working.
+    """
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < 3:  # if there are more than 2 non working ports stop the testing.
+        camera = cv.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working." % dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" % (dev_port, h, w))
+                working_ports.append((dev_port, h, w))
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not read." % (dev_port, h, w))
+                available_ports.append(dev_port)
+        dev_port += 1
+    return available_ports, working_ports, non_working_ports
+
+
 # ========================================
 pixels_per_cm_obj = 118.49  # text-with-aruco.png
 focal_length_x = 950.4602909088135
@@ -113,7 +154,7 @@ focal_length_y = 950.4602909088135
 camera_center_x = 640.0
 camera_center_y = 360.0
 distortion = np.array([0.09353041, -0.12232207, 0.00182885, -0.00131933, -0.30184632], dtype=np.float32) * 0
-use_external_cam = 0
+use_external_cam = select_cam_port()
 # ========================================
 
 parser = argparse.ArgumentParser(description='Code for calibration.')

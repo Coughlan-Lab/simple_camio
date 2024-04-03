322ee4fffab1123427bd6dca2464769ed5243b0a
Matteo Manzoni <matteo.manzoni2001@gmail.com>
2024-04-03 13:15:10 +0200

    added support for sift features 3D models

--- model/frame_processors/finger_sift_3d.py
+++ model/frame_processors/finger_sift_3d.py
@@ -1,8 +1,50 @@
 # mypy: ignore-errors
 from ..content import Content
 from .frame_processor import FrameProcessor
+from ..simple_camio_mp_3d import PoseDetectorMP3D
+from ..simple_camio_3d import SIFTModelDetector, CamIOPlayerOBJ, InteractionPolicyOBJ
+import cv2
+import numpy as np
 
 
 class FingerSift3DFP(FrameProcessor):
-    def __init__(self, content: Content, calibration_file: str) -> None:
-        FrameProcessor.__init__(self, content, calibration_file)
+    def get_audio_player(self):
+        return CamIOPlayerOBJ(self.content.as_dict())
+
+    def get_model_detector(self):
+        return SIFTModelDetector(self.content.as_dict(), self.intrinsic_matrix)
+
+    def get_interaction_policy(self):
+        return InteractionPolicyOBJ(self.content.as_dict(), self.intrinsic_matrix)
+
+    def get_pose_detector(self):
+        return PoseDetectorMP3D()
+
+    def process(self, img: np.ndarray) -> np.ndarray:
+        img = super().process(img)
+
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ok, rotation, translation = self.model_detector.detect(img_gray)
+
+        if not ok:
+            self.heartbeat_player.pause_sound()
+            self.crickets_player.play_sound()
+            return img
+        self.crickets_player.pause_sound()
+
+        img = self.annotator.annotate_image(img, [], rotation, translation)
+
+        self.interaction.project_vertices(rotation, translation)
+        gesture_loc, gesture_status, img = self.pose_detector.detect(img)
+
+        if gesture_loc is None:
+            self.heartbeat_player.pause_sound()
+            return img
+        self.heartbeat_player.play_sound()
+
+        if gesture_status != "moving":
+            img = self.annotator.draw_point_in_image(img, gesture_loc)
+
+        zone_id = self.interaction.push_gesture(gesture_loc)
+        self.audio_player.convey(zone_id, gesture_status)
+        return img

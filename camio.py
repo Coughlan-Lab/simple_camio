9389a78bf75b69e3d1b941509d5e0478b17b0a61
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-07-31 13:21:02 -0700

    nearest edge detection

--- camio.py
+++ camio.py
@@ -6,8 +6,10 @@ import pyglet.media
 
 from src.audio import AmbientSoundPlayer, CamIOPlayer
 from src.frame_processing import PoseDetector, SIFTModelDetector
+from src.graph import Coords, Graph
 from src.utils import *
 
+# Model loading
 parser = argparse.ArgumentParser(description="CamIO, with LLM integration")
 parser.add_argument(
     "--model",
@@ -15,19 +17,27 @@ parser.add_argument(
     default="models/UkraineMap/UkraineMap.json",
 )
 args = parser.parse_args()
+model = load_map_parameters(args.model)
 
-model = load_map_parameters(args.input1)
 
-cam_port = select_cam_port()
+# Node network graph
+graph = Graph(model["graph"])
+buffer = Buffer(5)
+min_corner, max_corner = graph.bounds
 
+# Frame processing
 model_detector = SIFTModelDetector(model["template_image"])
 pose_detector = PoseDetector()
+
+# Audio players
 camio_player = CamIOPlayer(model)
 camio_player.play_welcome()
 crickets_player = AmbientSoundPlayer(model["crickets"])
 heartbeat_player = AmbientSoundPlayer(model["heartbeat"])
 heartbeat_player.set_volume(0.05)
 
+# Video capture
+cam_port = select_cam_port()
 cap = cv2.VideoCapture(cam_port)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
@@ -37,15 +47,13 @@ if not ok:
     print("No camera image returned.")
     exit(0)
 
-timer = time.time() - 1
 
 camio_player.play_description()
-shape = cv2.imread(model["template_image"], cv2.IMREAD_COLOR).shape
 
-# Main loop
+timer = time.time() - 1
 while cap.isOpened():
 
-    cv2.imshow("image reprojection", frame)
+    cv2.imshow("CamIO", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     ret, frame = cap.read()
     if not ret:
@@ -57,9 +65,9 @@ while cap.isOpened():
     if waitkey == 27 or waitkey == ord("q"):
         break
 
-    prev_time = timer
-    timer = time.time()
-    elapsed_time = timer - prev_time
+    # prev_time = timer
+    # timer = time.time()
+    # elapsed_time = timer - prev_time
     # print("current fps: " + str(1/elapsed_time))
 
     pyglet.clock.tick()
@@ -74,20 +82,24 @@ while cap.isOpened():
         continue
     crickets_player.pause_sound()
 
-    gesture_loc, gesture_status, frame = pose_detector.detect(frame, rotation)
-    if gesture_loc is None:
+    gesture_position, gesture_status, frame = pose_detector.detect(frame, rotation)
+    if gesture_position is None:
         heartbeat_player.pause_sound()
         continue
-
-    if gesture_status == "pointing":
-        x = int(gesture_loc[0])
-        y = int(gesture_loc[1])
-
-        if 0 <= x < shape[1] and 0 <= y < shape[0]:
-            print("Gesture detected at x: " + str(x) + " y: " + str(y))
-            pass
-
     heartbeat_player.play_sound()
+
+    if gesture_status != "pointing":
+        continue
+
+    x = int(gesture_position[0])
+    y = int(gesture_position[1])
+
+    if min_corner[0] <= x < max_corner[0] and min_corner[1] <= y < max_corner[1]:
+        print("Gesture detected at x: " + str(x) + " y: " + str(y))
+        nearest_edge = graph.get_nearest_edge(Coords(x, y))
+        buffer.add(nearest_edge)
+
+        print("Nearest edge: " + str(nearest_edge))
 
 
 cap.release()

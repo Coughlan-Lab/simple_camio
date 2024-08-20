1abcc9a4fe7297560790e88d8c2c617ce651b1b8
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-08-20 11:48:04 -0700

    dict str better formatting

--- src/utils.py
+++ src/utils.py
@@ -9,25 +9,29 @@ import cv2
 import pyglet
 
 
+def str_format(v: Any) -> str:
+    return str(v).replace("_", " ")
+
+
 def str_dict(d: Dict[Any, Any], indent: int = 0) -> str:
     res = ""
 
     for key, value in d.items():
-        res += " " * indent + str(key) + ": "
+        res += " " * indent + str_format(key) + ": "
         if isinstance(value, dict):
             res += "\n" + str_dict(value, indent + 4)
         elif isinstance(value, list):
             if len(value) == 0:
                 res += "[]\n"
             elif len(value) == 1:
-                res += "[ " + str(value[0]) + " ]\n"
+                res += "[ " + str_format(value[0]) + " ]\n"
             else:
                 res += "[\n"
                 for item in value:
-                    res += " " * (indent + 4) + str(item) + ",\n"
+                    res += " " * (indent + 4) + str_format(item) + ",\n"
                 res += " " * indent + "]\n"
         else:
-            res += str(value) + "\n"
+            res += str_format(value) + "\n"
 
     return res
 

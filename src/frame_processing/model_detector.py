aead1c0c49aa4520dd163534faffb391fd59690b
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-09-04 13:38:00 -0700

    detection interval

--- src/frame_processing/model_detector.py
+++ src/frame_processing/model_detector.py
@@ -1,4 +1,5 @@
-from typing import Optional, Tuple
+import time
+from typing import Optional
 
 import cv2
 import numpy as np
@@ -6,6 +7,8 @@ import numpy.typing as npt
 
 
 class SIFTModelDetector:
+    DETECTION_INTERVAL = 5  # seconds
+
     def __init__(self, template_filename: str) -> None:
         # Load the template image
         img_template = cv2.imread(template_filename, cv2.IMREAD_GRAYSCALE)
@@ -16,7 +19,12 @@ class SIFTModelDetector:
             img_template, mask=None
         )
 
+        self.last_detection = 0.0, np.zeros((3, 3), dtype=np.float32)
+
     def detect(self, frame: npt.NDArray[np.uint8]) -> Optional[npt.NDArray[np.float32]]:
+        if time.time() - self.last_detection[0] < self.DETECTION_INTERVAL:
+            return self.last_detection[1]
+
         keypoints_scene, descriptors_scene = self.detector.detectAndCompute(frame, None)
         matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
         try:
@@ -49,4 +57,5 @@ class SIFTModelDetector:
             scene, obj, cv2.RANSAC, ransacReprojThreshold=8.0, confidence=0.995
         )
 
+        self.last_detection = time.time(), H
         return H
